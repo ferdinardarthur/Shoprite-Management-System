03ad40ace0b3ffe7a86f825bdcfd327c71111dc5
ferdinardarthur <farthur024@st.ug.edu.gh>
2022-08-28 17:09:25 -0400

    feat: User modules

---
+++ ShopriteManagementSystem/ManageCustomers.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace ShopriteManagementSystem
+{
+    partial class ManageCustomers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CustomernameTb = new System.Windows.Forms.RichTextBox();
+            this.CustomerPhoneTb = new System.Windows.Forms.RichTextBox();
+            this.Customerid = new System.Windows.Forms.RichTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.CustomersGV = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(2122, 267);
+            this.panel1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(2017, 0);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 65);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "X";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(717, 134);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(545, 65);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Manage Customers";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(446, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1027, 78);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SHOPRITE MANAGEMENT SYSTEM";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(81, 828);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(212, 37);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "CustomerPhone:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(85, 653);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(208, 37);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "CustomerName:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(133, 466);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 37);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "CustomerId:";
+            // 
+            // CustomernameTb
+            // 
+            this.CustomernameTb.Location = new System.Drawing.Point(311, 632);
+            this.CustomernameTb.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.CustomernameTb.Name = "CustomernameTb";
+            this.CustomernameTb.Size = new System.Drawing.Size(535, 79);
+            this.CustomernameTb.TabIndex = 16;
+            this.CustomernameTb.Text = "";
+            // 
+            // CustomerPhoneTb
+            // 
+            this.CustomerPhoneTb.Location = new System.Drawing.Point(311, 803);
+            this.CustomerPhoneTb.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.CustomerPhoneTb.Name = "CustomerPhoneTb";
+            this.CustomerPhoneTb.Size = new System.Drawing.Size(535, 81);
+            this.CustomerPhoneTb.TabIndex = 15;
+            this.CustomerPhoneTb.Text = "";
+            // 
+            // Customerid
+            // 
+            this.Customerid.Location = new System.Drawing.Point(311, 444);
+            this.Customerid.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.Customerid.Name = "Customerid";
+            this.Customerid.Size = new System.Drawing.Size(535, 77);
+            this.Customerid.TabIndex = 14;
+            this.Customerid.Text = "";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(1351, 364);
+            this.label8.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(449, 65);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "CUSTOMERS LIST";
+            // 
+            // CustomersGV
+            // 
+            this.CustomersGV.BackgroundColor = System.Drawing.Color.White;
+            this.CustomersGV.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.CustomersGV.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.CustomersGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CustomersGV.Location = new System.Drawing.Point(994, 431);
+            this.CustomersGV.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.CustomersGV.Name = "CustomersGV";
+            this.CustomersGV.RowHeadersWidth = 82;
+            this.CustomersGV.RowTemplate.Height = 41;
+            this.CustomersGV.Size = new System.Drawing.Size(1055, 870);
+            this.CustomersGV.TabIndex = 20;
+            this.CustomersGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.CustomersGV_CellContentClick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(485, 1071);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 47);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "HOME";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(697, 982);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 47);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "DELETE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(504, 982);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 47);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "EDIT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 982);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 47);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ManageCustomers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2122, 1360);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.CustomersGV);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.CustomernameTb);
+            this.Controls.Add(this.CustomerPhoneTb);
+            this.Controls.Add(this.Customerid);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ManageCustomers";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ManageCustomers";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomersGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private RichTextBox CustomernameTb;
+        private RichTextBox CustomerPhoneTb;
+        private RichTextBox Customerid;
+        private Label label8;
+        private DataGridView CustomersGV;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+    }
+}
